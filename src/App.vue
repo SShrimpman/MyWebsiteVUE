f629747137fabe60668dfb9b0ea777f9a831a3e3
António Camarão <camaraoacsn@gmail.com>
2022-10-28 15:33:52 +0100

    Base Website Done

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+<Base/>
+</template>
+
+
+<script setup>
+import Base from '../src/components/Base.vue'
+</script>
---
+++ src/components/Base.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-center font-bold text-2xl">
+        Olá
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
